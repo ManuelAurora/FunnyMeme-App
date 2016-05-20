369c038dba39a868c0f24b372174bc4aed2faeb5
Manuel <naruto06@mail.ru>
2016-05-20 10:43:58 +0300

    Refactored: Layout in landscape mode, Image View

--- Meme-App/MemeEditorViewController.swift
+++ Meme-App/MemeEditorViewController.swift
@@ -103,8 +103,8 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     
     // MARK: *** Notification functions ***
     func subscribeToKeyboardNotification() {
-       NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
-       NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+       NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MemeEditorViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+       NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MemeEditorViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
     }
     
     func unsubscribeToKeyboardNotification() {
